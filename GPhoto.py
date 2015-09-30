08c6e20b3df6cf9d17511a500bb7b1a03658be49
Jan Kurik <jkurik@redhat.com>
2015-09-30 20:25:19 +0200

    Implemented check for presence of a file on GDrive

--- GPhoto.py
+++ GPhoto.py
@@ -26,6 +26,7 @@ class GPhoto(object):
         self.store = None
         self.creds = None
         self.service = None
+        self.cache = {}
 
     def auth(self, oauth2json = None, oauth2storage = None):
         if oauth2json is not None:
@@ -70,11 +71,49 @@ class GPhoto(object):
         except apiclient.errors.HttpError as error:
             return None
 
+    def get_child_id(self, title, parent_id):
+        page_token = None
+        while True:
+            try:
+                param = {}
+                if page_token:
+                    param['pageToken'] = page_token
+                children = self.service.children().list(folderId = parent_id, **param).execute()
+                for child in children.get('items', []):
+                    ch = self.service.files().get(fileId = child['id']).execute()
+                    if ch['title'] == title:
+                        return child['id']
+                page_token = children.get('nextPageToken')
+                if not page_token:
+                    break
+            except apiclient.errors.HttpError as error:
+                raise
+        return None
+
+    def file_exists(self, file_name, root_dir = 'root'):
+        fn = file_name.split('/')
+        child_id = self.get_child_id(fn[0], root_dir)
+        if len(fn) == 1:
+            # Check existence of the file
+            return child_id is not None
+        # Go one level deeper
+        fn.pop(0)
+        return self.file_exists('/'.join(fn), child_id)
+            
+
 if __name__ == "__main__":
     oauth2json = os.path.expanduser('~/.gp.json')
     oauth2storage = os.path.expanduser('~/.gp')
     gp = GPhoto(oauth2json = oauth2json, oauth2storage = oauth2storage)
     gp.auth()
-    d = gp.create_dir("BufGuf")
+    #d = gp.create_dir("BufGuf")
     #gp.upload_file(os.path.expanduser('~/fscheck.sh'), d['id'])
-    gp.upload_file(os.path.expanduser('~/readiness-f23-alpha.txt'), d['id'])
+    #gp.upload_file(os.path.expanduser('~/readiness-f23-alpha.txt'), d['id'])
+    #print 'U-Temp', gp.file_exists('U-Temp')
+    #print 'Work/Links', gp.file_exists('Work/Links')
+    #print 'Personal', gp.file_exists('Personal')
+    #print 'Personal/Blbost', gp.file_exists('Personal/Blbost')
+    #print 'Pictures/Foto/2015/07/29', gp.file_exists('Pictures/Foto/2015/07/29')
+    print 'Pictures/Foto/2015/07/29/IMG_2552.JPG', gp.file_exists('Pictures/Foto/2015/07/29/IMG_2552.JPG')
+    print 'Pictures/Foto/2015/07/29/IMG_2552.jpg', gp.file_exists('Pictures/Foto/2015/07/29/IMG_2552.jpg')
+    print 'Pictures/Foto/2015/07/29/IMG2552.JPG', gp.file_exists('Pictures/Foto/2015/07/29/IMG2552.JPG')
