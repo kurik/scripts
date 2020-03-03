2969f70806af8485200825117c08673e833ad052
Jan Kurik <jkurik@redhat.com>
2020-03-03 10:35:36 +0100

    Prolonged timeout of xget

--- xget.sh
+++ xget.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-while ! wget --no-netrc --no-use-server-timestamps -T 5 --no-proxy --prefer-family=IPv4 -c "${X}"; do sleep 1; done
+while ! wget --no-netrc --no-use-server-timestamps -T 10 --no-proxy --prefer-family=IPv4 -c "${X}"; do sleep 1; done
 
 return $?
